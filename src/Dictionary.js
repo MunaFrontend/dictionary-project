072a6da1c2be9a63380a5a5c563efc857b06195e
MunaFrontend <minaality@gmail.com>
2023-08-30 10:04:39 +0200

    added components

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,29 +1,34 @@
 import React, { useState } from "react";
-import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
+import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-  function handleResponse(response){
-    console.log(response)
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
-    let apiurl = `https://api.dictionaryapi.dev/api/v2/entries/en_us ${keyword};
-    axios.get(apiurl).then(handleResponse)`
+
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
- 
+
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
