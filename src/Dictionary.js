68249565abf9c041acbc746a2405d1a9902b1690
MunaFrontend <minaality@gmail.com>
2023-08-30 10:01:16 +0200

    added search bar

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  function handleResponse(response){
+    console.log(response)
+  }
 
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
+    let apiurl = `https://api.dictionaryapi.dev/api/v2/entries/en_us ${keyword};
+    axios.get(apiurl).then(handleResponse)`
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-
+ 
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
